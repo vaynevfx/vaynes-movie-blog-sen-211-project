920d9639cdaa61783b954bede89d93c6658113e7
vaynevfx <98434311+vaynevfx@users.noreply.github.com>
2022-01-29 02:59:51 +0100

    sen211
    sen211

---
+++ index.php
@@ -0,0 +1,157 @@
+<?php require("libs/fetch_data.php");?>
+<!DOCTYPE html>
+<html lang="zxx">
+<head>
+	<title>Welcome | VAYNE's Movie Blog </title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="utf-8">
+		<meta charset="utf-8" name="description" content="<?php getshortdescription("titles");?>">
+	<meta name="keywords" content="<?php getkeywords("titles");?>" />
+	<script>
+		addEventListener("load", function () {
+			setTimeout(hideURLbar, 0);
+		}, false);
+
+		function hideURLbar() {
+			window.scrollTo(0, 1);
+		}
+	</script>
+	<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+	<link rel="stylesheet" href="css/jquery.desoslide.css">
+	<link href="css/style.css" rel='stylesheet' type='text/css' />
+	<link href="css/fontawesome-all.css" rel="stylesheet">
+	<link href="//fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800"
+	rel="stylesheet">
+</head>
+
+<body>
+	<?php include("header.php");?>
+	<center><h2>Trending Movie</h2></center>
+	<img src="images/spider1.jpg" height="100%" width="100%">
+	<p>With Spider-Man's identity now revealed, our friendly neighborhood web-slinger is unmasked and no longer able to separate his normal life as Peter Parker from the high stakes of being a superhero. When Peter asks for help from Doctor Strange, the stakes become even more dangerous, forcing him to discover what it truly means to be Spider-Man.s</p>
+	<center>
+	      <div class="fakeimg" style="height:350px;"><iframe width="480" height="270"
+src="https://www.youtube.com/embed/JfVOs4VSpmA">
+</iframe></div></center>
+      <p>With a stunning jawdropping trailer without a lot left to be desired seems like the sky is the limit for Tom Holland's last stand as the friendly neighborhood spider-man..</p>
+      
+<div>
+					<!--//main-->
+					<?php include("footer.php");?>
+					<!---->
+					<!-- js -->
+					<script src="js/jquery-2.2.3.min.js"></script>
+					<!-- //js -->
+					<!-- desoslide-JavaScript -->
+					<script src="js/jquery.desoslide.js"></script>
+					<script>
+						$('#demo1_thumbs').desoSlide({
+							main: {
+								container: '#demo1_main_image',
+								cssClass: 'img-responsive'
+							},
+							effect: 'sideFade',
+							caption: true
+						});
+					</script>
+
+					<!-- requried-jsfiles-for owl -->
+					<script>
+						$(window).load(function () {
+							$("#flexiselDemo1").flexisel({
+								visibleItems: 3,
+								animationSpeed: 1000,
+								autoPlay: true,
+								autoPlaySpeed: 3000,
+								pauseOnHover: true,
+								enableResponsiveBreakpoints: true,
+								responsiveBreakpoints: {
+									portrait: {
+										changePoint: 480,
+										visibleItems: 1
+									},
+									landscape: {
+										changePoint: 640,
+										visibleItems: 2
+									},
+									tablet: {
+										changePoint: 768,
+										visibleItems: 3
+									}
+								}
+							});
+
+						});
+					</script>
+					<script>
+						$(window).load(function () {
+							$("#flexiselDemo2").flexisel({
+								visibleItems: 3,
+								animationSpeed: 1000,
+								autoPlay: true,
+								autoPlaySpeed: 3000,
+								pauseOnHover: true,
+								enableResponsiveBreakpoints: true,
+								responsiveBreakpoints: {
+									portrait: {
+										changePoint: 480,
+										visibleItems: 1
+									},
+									landscape: {
+										changePoint: 640,
+										visibleItems: 2
+									},
+									tablet: {
+										changePoint: 768,
+										visibleItems: 3
+									}
+								}
+							});
+
+						});
+					</script>
+					<script src="js/jquery.flexisel.js"></script>
+					<!-- //password-script -->
+					<!--/ start-smoth-scrolling -->
+					<script src="js/move-top.js"></script>
+					<script src="js/easing.js"></script>
+					<script>
+						jQuery(document).ready(function ($) {
+							$(".scroll").click(function (event) {
+								event.preventDefault();
+								$('html,body').animate({
+									scrollTop: $(this.hash).offset().top
+								}, 900);
+							});
+						});
+					</script>
+					<!--// end-smoth-scrolling -->
+
+					<script>
+						$(document).ready(function () {
+							
+							var defaults = {
+							  			containerID: 'toTop', // fading element id
+										containerHoverID: 'toTopHover', // fading element hover id
+										scrollSpeed: 1200,
+										easingType: 'linear' 
+									};
+									
+
+									$().UItoTop({
+										easingType: 'easeOutQuart'
+									});
+
+								});
+							</script>
+							<a href="#home" class="scroll" id="toTop" style="display: block;">
+								<span id="toTopHover" style="opacity: 1;"> </span>
+							</a>
+
+							<!-- //Custom-JavaScript-File-Links -->
+							<script src="js/bootstrap.js"></script>
+
+
+						</body>
+
+						</html>
